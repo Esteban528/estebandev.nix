37f18480384f144747d72e162d6bb9f3328d5adb
Esteban Gonzalez Florez <estebangonza2005@gmail.com>
2024-08-22 22:22:15 -0500

    feat: 🎨 add reload tasks

--- home/estebandev/modules/programs/eww/config/scripts/widget.sh
+++ home/estebandev/modules/programs/eww/config/scripts/widget.sh
@@ -6,6 +6,7 @@ STATUS_FILE="${FILE}.status"
 if [ ! -f "$STATUS_FILE" ]; then
   eww open widget
   touch $STATUS_FILE
+  eww update tasks="$(bash /home/estebandev/Dev/bin/Calendar/todo.sh)"
 else
   rm "$STATUS_FILE"
   eww close widget
