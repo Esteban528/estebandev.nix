dbe430c047ce1fb3d69c62ee07fa7e293d9570c7
Esteban Gonzalez Florez <estebangonza2005@gmail.com>
2024-09-04 20:37:59 -0500

    feat: update

--- home/estebandev/modules/programs/stylix/default.nix
+++ home/estebandev/modules/programs/stylix/default.nix
@@ -17,7 +17,7 @@
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
     base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-material-dark-hard.yaml";
 
-    image = ./assets/wallpaper.jpg;
+    image = ./assets/gruvboxwlp.png;
 
     cursor.package = pkgs.bibata-cursors;
     cursor.name = "Bibata-Modern-Ice";
