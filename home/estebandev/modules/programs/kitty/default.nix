c03ea3c85425f434357cb8fe593faa11ec0f00e1
Esteban Gonzalez Florez <estebangonza2005@gmail.com>
2024-09-04 20:30:39 -0500

    feat: update

--- home/estebandev/modules/programs/kitty/default.nix
+++ home/estebandev/modules/programs/kitty/default.nix
@@ -16,7 +16,7 @@
       cursor_underline_thickness = 7;
       mouse_hide_wait = "3.0";
       window_padding_width = 6;
-      background_opacity = lib.mkForce "0.9899";
+      background_opacity = lib.mkForce "0.9799";
       # background_color = lib.mkForce "#${config.lib.stylix.colors.base0D}";
     };
 
