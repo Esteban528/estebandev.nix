187c1c61a55f85c2546d55a5b88e8ecc3dfcbf71
Esteban Gonzalez Florez <estebangonza2005@gmail.com>
2024-09-14 14:10:56 -0500

    feat: update

--- hosts/pelusa/configuration.nix
+++ hosts/pelusa/configuration.nix
@@ -74,7 +74,6 @@
     xwayland.enable = true;
   };
 
-  desktopManager.gnome.enable = true;
   # xdg.portal = {
   #   enable = true;
   #   xdgOpenUsePortal = true;
@@ -82,26 +81,7 @@
   #   config.common.default = ["gtk"];
   # };
 
-  environment.gnome.excludePackages =
-    (with pkgs; [
-      gnome-photos
-      gnome-tour
-    ])
-    ++ (with pkgs.gnome; [
-      cheese # webcam tool
-      gnome-music
-      gnome-terminal
-      gedit # text editor
-      epiphany # web browser
-      geary # email reader
-      evince # document viewer
-      gnome-characters
-      totem # video player
-      tali # poker game
-      iagno # go game
-      hitori # sudoku game
-      atomix # puzzle game
-    ]);
+
 
   # services.displayManager.sddm.package = pkgs.kdePackages.sddm;
   # services.displayManager.sddm.enable = true;
