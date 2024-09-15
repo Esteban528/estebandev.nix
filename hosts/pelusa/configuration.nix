6a8626ac8c18874946a171f26b369fbc41571d1f
Esteban Gonzalez Florez <estebangonza2005@gmail.com>
2024-09-15 12:43:11 -0500

    feat: update

--- hosts/pelusa/configuration.nix
+++ hosts/pelusa/configuration.nix
@@ -136,7 +136,7 @@
     QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
     QT_AUTO_SCREEN_SCALE_FACTOR = "1";
 
-    SDL environment variable
+    #SDL environment variable
     SDL_VIDEODRIVER = "wayland";
 
     # Other environment variables
