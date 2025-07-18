d7b43367829e49144142da2ef49be52971efa595
Esteban Gonzalez <estebangonza2005@gmail.com>
2025-07-18 14:05:49 -0500

    feat: update

--- home/estebandev/modules/programs/niri/default.nix
+++ home/estebandev/modules/programs/niri/default.nix
@@ -7,7 +7,7 @@
     dir = "niri";
     colors = config.lib.stylix.colors;
     screenshot-path = "~/Pictures/Screenshots/Screenshot_%Y-%m-%d %H-%M-%S.png";
-    radius = "8";
+    radius = "3";
   in{
 
   home.file = {
