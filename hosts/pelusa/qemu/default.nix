c78d508891acea32538a3ee143683a71a2a2b7b9
Esteban Gonzalez <102267595+Esteban528@users.noreply.github.com>
2025-08-13 09:35:03 -0500

    feat: update

--- hosts/pelusa/qemu/default.nix
+++ hosts/pelusa/qemu/default.nix
@@ -1,30 +1,34 @@
 {pkgs, ...} : {
   systemd.tmpfiles.rules = [
     "L+ /var/lib/qemu/firmware - - - - ${pkgs.qemu}/share/qemu/firmware"
-    "L+ /usr/libexec/virtiofsd - - - - ${pkgs.virtiofsd}/bin/virtiofsd"
+    #"L+ /usr/libexec/virtiofsd - - - - ${pkgs.virtiofsd}/bin/virtiofsd"
   ];
-  virtualisation.libvirtd = {
-    enable = true;
-    qemu = {
-      package = pkgs.qemu_kvm;
-      runAsRoot = true;
-      swtpm.enable = true;
-      ovmf = {
-        enable = true;
-        packages = [(pkgs.OVMF.override {
-          secureBoot = true;
-          tpmSupport = true;
-        }).fd];
-      };
-    };
-  };
-  programs.virt-manager.enable = true;
 
-  users.groups.libvirtd.members = ["estebandev"];
-  virtualisation.spiceUSBRedirection.enable = true;
+  # virtualisation.libvirtd = {
+  #   enable = true;
+  #   qemu = {
+  #     package = pkgs.qemu_kvm;
+  #     runAsRoot = true;
+  #     swtpm.enable = true;
+  #     ovmf = {
+  #       enable = true;
+  #       packages = [(pkgs.OVMF.override {
+  #         secureBoot = true;
+  #         tpmSupport = true;
+  #       }).fd];
+  #     };
+  #   };
+  # };
+
+  #users.groups.libvirtd.members = ["estebandev"];
+  # virtualisation.spiceUSBRedirection.enable = true;
 
   environment.systemPackages = with pkgs; [
     qemu
-    virtiofsd
   ];
+
+  # boot.binfmt.emulatedSystems = [
+  #   "aarch64-linux"
+  #   "riscv64-linux"
+  # ];
 }
