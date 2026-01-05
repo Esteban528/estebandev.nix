89e3722a4cc0e67328699ed19ad69dba7af8ef44
Esteban Gonzalez <estebangonza2005@gmail.com>
2026-01-05 10:33:51 -0500

    chore: update

--- hosts/laptop/misc.nix
+++ hosts/laptop/misc.nix
@@ -1,5 +1,43 @@
-{...} : {
-  # Enable CUPS to print documents.
-  #services.printing.enable = true;
-
+# In /etc/nixos/configuration.nix
+{ pkgs, lib, options, ...}:
+{
+  ## To use, put this in your configuration, switch to it, and restart NM:
+  ## $ sudo systemctl restart NetworkManager.service
+  ## To check if it works, you can do `sudo systemctl status systemd-timesyncd.service`
+  ## (it may take a bit of time to pick the right NTP as it may try the
+  ## other NTP firsts)
+  networking.networkmanager.dispatcherScripts = [
+    {
+      # https://wiki.archlinux.org/title/NetworkManager#Dynamically_set_NTP_servers_received_via_DHCP_with_systemd-timesyncd
+      # You can debug with sudo journalctl -u NetworkManager-dispatcher -e
+      # make sure to restart NM as described above
+      source = pkgs.writeText "10-update-timesyncd" ''
+        [ -z "$CONNECTION_UUID" ] && exit 0
+        INTERFACE="$1"
+        ACTION="$2"
+        case $ACTION in
+        up | dhcp4-change | dhcp6-change)
+            systemctl restart systemd-timesyncd.service
+            if [ -n "$DHCP4_NTP_SERVERS" ]; then
+              echo "Will add the ntp server $DHCP4_NTP_SERVERS"
+            else
+              echo "No DHCP4 NTP available."
+              exit 0
+            fi
+            mkdir -p /etc/systemd/timesyncd.conf.d
+            # <<-EOF must really use tabs to keep indentation correct… and tabs are often converted to space in wiki
+            # so I don't want to risk strange issues with indentation
+            echo "[Time]" > "/etc/systemd/timesyncd.conf.d/''${CONNECTION_UUID}.conf"
+            echo "NTP=$DHCP4_NTP_SERVERS" >> "/etc/systemd/timesyncd.conf.d/''${CONNECTION_UUID}.conf"
+            systemctl restart systemd-timesyncd.service
+            ;;
+        down)
+            rm -f "/etc/systemd/timesyncd.conf.d/''${CONNECTION_UUID}.conf"
+            systemctl restart systemd-timesyncd.service
+            ;;
+        esac
+        echo 'Done!'
+      '';
+    }
+  ];
 }
