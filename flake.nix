ed93c2cfd836407f33c84621d66d110f76f5e8df
Esteban Gonzalez <estebangonza2005@proton.me>
2026-02-21 13:14:17 -0500

    chore: update

--- flake.nix
+++ flake.nix
@@ -24,6 +24,7 @@
             home-manager.nixosModules.home-manager
             {
               home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
               home-manager.users.estebandev = {
                 imports = [
                   ./home/estebandev/home.nix
