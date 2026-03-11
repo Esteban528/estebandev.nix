23aded2ae9d9a19bf67e8dca28ec71da77d7d975
Esteban Gonzalez <estebangonza2005@proton.me>
2026-03-11 15:02:18 -0500

    chore: update

--- home/estebandev/modules/programs/mpv.nix
+++ home/estebandev/modules/programs/mpv.nix
@@ -1,25 +1,17 @@
-{...} : {
+{pkgs, ...} : {
 
-  programs.mpv = {
-    enable = true;
+programs.mpv = {
+  enable = true;
 
-    package = (
-      pkgs.mpv-unwrapped.wrapper {
-        scripts = with pkgs.mpvScripts; [
-          uosc
-          sponsorblock
-        ];
+  scripts = with pkgs.mpvScripts; [
+    uosc
+    sponsorblock
+  ];
 
-        mpv = pkgs.mpv-unwrapped.override {
-          waylandSupport = true;
-        };
-      }
-    );
-
-    config = {
-      profile = "high-quality";
-      ytdl-format = "bestvideo+bestaudio";
-      cache-default = 4000000;
-    };
+  config = {
+    profile = "high-quality";
+    ytdl-format = "bestvideo+bestaudio";
+    cache-default = 4000000;
   };
+};
 }
